37a646ec9dbadbc2f435d626e548739745a666dc
Madhav Goyal <57744109+MadhavGoyal-6@users.noreply.github.com>
2023-04-08 19:04:00 +0530

    👀 Make tracker class

--- include/Tracker/Tracker.h
+++ include/Tracker/Tracker.h
@@ -2,13 +2,34 @@
 #ifndef TRACKER_H
 #define TRACKER_H
 
+#include "Serializer/Serializer.h"
+
 #include <Windows.h>
 #include <fileapi.h>
+#include <libloaderapi.h>
+#include <filesystem>
+#include <vector>
+
+#define SINGLE 0
+#define MULTI 1
+#define MAX_PATH_SIZE 512
+
+#ifdef DEBUG_TRACKER
+#define PRINT(x) std::cout << x
+#endif // DEBUG_TRACKER
 
 class Tracker
 {
+	int mode = MULTI;
+	folder = new char[MAX_PATH_SIZE];
+	Serializer *ser;
 public:
-	Tracker(char* filepath);
+	Tracker(char* folder);
+	void directoryHandler(char* path);
+	void getFileNames();
+	void SaveFileHash(char* filename);
+	Serializer* getSerializer();
 };
+	void ErrorExit(LPTSTR lpszFunction);
 
 #endif // !TRACKER_H
--- src/Tracker/Tracker.cpp
+++ src/Tracker/Tracker.cpp
@@ -1,6 +1,103 @@
 #include "Tracker/Tracker.h"
-#include <iostream>
+#include "SHA1/SHA1.h"
 
-Tracker::Tracker(char* filepath) {
-	std::wcout << (GetFileAttributesA(filepath) == FILE_ATTRIBUTE_DIRECTORY ? "True" : "False") << std::endl;
+#include <iostream>
+#include <strsafe.h>
+
+Tracker::Tracker(char* path) {
+	switch (GetFileAttributesA(path)) {
+	case FILE_ATTRIBUTE_DIRECTORY: // For Directory Input
+        directoryHandler(path);
+		break;
+	
+    case FILE_ATTRIBUTE_ARCHIVE: // For Single file Input
+        SaveFileHash(path);
+		break;
+	
+    default:
+		std::cout << "Invalid Path provided" << std::endl;
+	};
+}
+
+void Tracker::directoryHandler(char* path) {
+    SetCurrentDirectoryA(path);
+
+#ifdef DEBUG_TRACKER
+    char* a = new char[MAX_PATH_SIZE];
+    GetCurrentDirectoryA(MAX_PATH_SIZE, a);
+    PRINT("Current Directory is: " << a << std::endl);
+#endif // DEBUG_TRACKER
+
+    this->folder = path;
+    this->ser = new Serializer(std::filesystem::path(std::string(this->folder)));
+    getFileNames();
+}
+
+void Tracker::getFileNames() {
+    std::vector<std::filesystem::path> files;
+    for (const auto& file : std::filesystem::directory_iterator(this->folder)) {
+
+#ifdef DEBUG_TRACKER
+		std::cout << file.path() << std::endl;
+#endif // DEBUG_TRACKER
+
+        files.push_back(file.path());
+	}
+}
+
+void Tracker::SaveFileHash(char* filename) {
+    this->ser = new Serializer(std::filesystem::path(std::string(this->folder)));
+    Message contents(&filename.string()[0], true);
+    SHA1 context(&contents);
+    context.createDigest();
+
+#ifdef DEBUG_TRACKER
+    std::cout << digestString(context.getDigest()) << std::endl;
+#endif // DEBUG_TRACKER
+
+    ser->update(filename.string(), context.getDigest());
+    ser->serialize();        
+}
+
+Serializer* Tracker::getSerializer() {
+    return ser;
+}
+
+
+
+// Debug code copied from Microsoft Docs
+
+#include <strsafe.h>
+
+void ErrorExit(LPTSTR lpszFunction)
+{
+    // Retrieve the system error message for the last-error code
+
+    LPVOID lpMsgBuf;
+    LPVOID lpDisplayBuf;
+    DWORD dw = GetLastError();
+
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        dw,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf,
+        0, NULL);
+
+    // Display the error message and exit the process
+
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+        (lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+    StringCchPrintf((LPTSTR)lpDisplayBuf,
+        LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+        TEXT("%s failed with error %d: %s"),
+        lpszFunction, dw, lpMsgBuf);
+    MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
+
+    LocalFree(lpMsgBuf);
+    LocalFree(lpDisplayBuf);
+    ExitProcess(dw);
 }
